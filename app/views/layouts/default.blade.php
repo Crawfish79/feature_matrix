a362d61e920120050e3056bb3269395b3268c583
webdevdea <webdevdea@gmail.com>
2014-09-02 17:38:12 -0400

    Added Dynamix clickable image
    click on the image it takes you to the dynamic site

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -2,58 +2,44 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Dynamix Feature Matrix::Home</title>
+	<title>Laravel PHP Framework</title>
 	<style>
-		  table {
-		  		border:3;
-		  		width:835;
-				cellpadding:3; 
-		  		cellspacing:0;
-		  		margin-left:auto;
-		  		margin-right:auto;
-		  }
-		  		
-          td {
-          		background-color:#f8f8f8;
-          } 
-          		
-  		  caption {
-  		  		font-family:Verdana, sans-serif; 
-  		  		font-weight:bold; 
-  		  		font-size:1em; 
-  		  		padding-bottom:5px;
-  		  		color:white;
-  		  		background-color:#888888;
-  		  }
-  		  
-		  ul {
-			    list-style-type: none;
-			    margin: 0;
-			    padding: 0;
-		  }
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
 			
-		  li {
-			    display: inline;
-		  }
-		    		  
+			color: #999;
+		}
+
+		.welcome {
+			width: 300px;
+			height: 200px;
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			margin-left: -150px;
+			margin-top: -100px;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
 	</style>
 </head>
-	<body>	
-		<div style="width: 100%; overflow: hidden;padding:2px;">
-		    <div style="width: 600px; float: left;">
-		    	<h1>DynamiX Feature Matrix</h1>
-		    </div>
-		    <div style="margin-left: 620px;margin-top:35px; text-align: right;"> 
-		    	<label>search: <input type="text"></label>&nbsp;
-		    	<input type="submit" value="go">
-			</div>
-		</div>
-		<div style = "border: 1px solid">
-			<ul>
-			  <li><a href="">Websites</a></li>
-			  <li><a href="">Feature</a></li>
-			</ul>
-		</div><br>
-		@yield('content')
-	</body>
+<div class="welcome">
+		<a href="http://www.dynamixwebdesign.com/" title="Dynamix Feature Matrix"><img src="http://www.dynamixwebdesign.com/images/DynamiX-logo.png">
+
+
+   @yield('content')
+   
+</div>
+
+</body>
 </html>
