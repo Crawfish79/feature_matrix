3c437ce0bf4bfd6fdae9d85b8d72cbd89e5faa60
DeAnna Lynn Riddlespur <webdevdea@gmail.com>
2014-09-28 08:01:42 -0400

    Update create_site.blade.php
    Updated Style

--- app/views/edit/create_site.blade.php
+++ app/views/edit/create_site.blade.php
@@ -1,17 +1,14 @@
 
 @extends('layouts.default')
 @section('content')
-			<section class="header section-padding">
-					<div class="background">&nbsp;</div>
-					<div style="text-align:center">
-					<div class="container">
-						<div class="header-text">
-								<h1>Add A Site</h1>
-							</div>
+		<div class = 'panels'>
+		<div class="panel panel-primary">
+			<div class="panel-body bg-warning">
+									<div class="panel-heading"<h1>Add A Site</h1></div>
+						
 								
-						</div>
-					</div>
-				</section>
+					
+		
 			
 					
 					
@@ -23,28 +20,25 @@
 					
 					
 					{{ Form::label('siteName', 'WebSite') }}
-					{{ Form::text('siteName', null,['class'=>'form-control']) }}
+					<div class="col-sm-4">{{ Form::text('siteName', null,['placeholder'=>'http://www.example.com','class'=>'form-control']) }}</div>
+				
 					</div>
 					
 					<div class="form-group">
-				
-				
-					{{ Form::label('description', 'description') }}
-					{{ Form::text('description', null,['class'=>'form-control']) }}
+						{{ Form::label('description', 'Description') }}
+					<div class="col-sm-4">{{ Form::text('description', null,['placeholder'=>'This site ...','class'=>'form-control']) }}</div>
 						</div>
-								
+						
 					<div class="form-group">
-				
-				
-					{{ Form::label('launchDate', 'Launch Date ') }}
-					{{ Form::text('launchDate', null,['placeholder'=>'0000-00-00','class'=>'form-control']) }}
+						{{ Form::label('launchDate', 'Launch Date ') }}
+					<div class="col-sm-4">{{ Form::text('launchDate', null,['placeholder'=>'0000-00-00','class'=>'form-control']) }}</div>
 					</div>
-				
 				<div class="form-group">
-					{{ Form::submit('Create Site Listing', ['class'=>'btn btn-primary']) }}
+						<div class="col-sm-offset-2 col-sm-10">{{ Form::submit('Create Site Listing', ['class'=>'btn btn-primary']) }}</div>
 				</div>
 				{{ Form::close() }}
 					</div>
+				</div>
 			
 			</div>
 @stop
