d5b8f5b7499704843240b493a4e3ac3d80080307
DeAnna Lynn Riddlespur <webdevdea@gmail.com>
2014-10-28 10:47:20 -0400

    Update clientProfiles.blade.php
    Add Edit SIte

--- app/views/profiles/clientProfiles.blade.php
+++ app/views/profiles/clientProfiles.blade.php
@@ -7,6 +7,10 @@
 		<div class="panel panel-primary">
 			<div class="panel-heading"><h3 class = "panel-title">{{$client->siteName}}</h3></div>
 		    <div class="panel-body">{{$client->description}}</div>
+		       <div class="panel-body">   <a href="{{ action('ClientSiteController@edit',$client->clientID) }}"
+                                        class="btn btn-info">Edit</a>
+                                        
+                                       </div>
 		</div>
 	</div>
 	<!--forms group-->
