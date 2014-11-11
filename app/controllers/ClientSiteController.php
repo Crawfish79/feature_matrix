4e24989a08ad3bf2c31f177bd4d24df4cbaf84e3
DeAnna Lynn Riddlespur <webdevdea@gmail.com>
2014-11-11 18:26:08 -0500

    Fixed styling
    I messed it up when I copied and pasted.. no code changed only the indentation

--- app/controllers/ClientSiteController.php
+++ app/controllers/ClientSiteController.php
@@ -6,27 +6,27 @@ class ClientSiteController extends BaseController
 {
     public function showClients()
 {
-$clients = ClientSite::all();
-return View::make('index')->with('clients',$clients);
+            $clients = ClientSite::all();
+        return View::make('index')->with('clients',$clients);
 }
 //returns client and client feature info for profile
-public function clientProfile($siteName)
+    public function clientProfile($siteName)
 {   //select * from clientSites where siteName = $siteName
-$client = ClientSite::whereSitename($siteName)->first();    
-$clientID = $client['clientID'];//getting clientID for use in join query
+            $client = ClientSite::whereSitename($siteName)->first();    
+            $clientID = $client['clientID'];//getting clientID for use in join query
 //queries for retrieving features of each group for the selected client
-$clientFeatures = DB::table('features')->join('clientFeatures', function($join) use($clientID)
+            $clientFeatures = DB::table('features')->join('clientFeatures', function($join) use($clientID)
 {
-$join->on( 'clientFeatures.featureID', '=', 'features.featureID')   
-->where('clientFeatures.clientID', '=', $clientID);
-})->get();
-$groupIDs = array();
-foreach ($clientFeatures as $c) {
-$groupIDs[] = $c->groupID;
+            $join->on( 'clientFeatures.featureID', '=', 'features.featureID')   
+            ->where('clientFeatures.clientID', '=', $clientID);
+            })->get();
+            $groupIDs = array();
+            foreach ($clientFeatures as $c) {
+$           groupIDs[] = $c->groupID;
 }
-return View::make('profiles.clientProfiles')->with('client',$client)
-->with('groupIDs',$groupIDs)
-->with('clientFeatures',$clientFeatures);   
+        return View::make('profiles.clientProfiles')->with('client',$client)
+        ->with('groupIDs',$groupIDs)
+        ->with('clientFeatures',$clientFeatures);   
 }   
          
          public function create()
